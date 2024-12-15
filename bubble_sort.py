6f02acc4e78b562e3bd6507addd10bcdb5c9f8e1
Gokul07745 <95127666+Gokul07745@users.noreply.github.com>
2024-12-15 10:29:06 +0530

    Delete bubble_sort.py

--- bubble_sort.py
+++
@@ -1,8 +0,0 @@
-def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(0, len(arr)-i-1): # for every iteration it starts from the first element of the array and compares adjacent element
-            if arr[j]>arr[j+1]:
-                arr[j+1],arr[j]=arr[j],arr[j+1]
-    return arr
-arr=list(map(int,input().split()))
-print(bubble_sort(arr))
